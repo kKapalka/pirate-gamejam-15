9fb1744af9ac272f2f3da2d293a3a7c5fadbf327
kKapalka <k.kapalka@hotmail.com>
2024-07-21 11:15:49 +0200

    ui input used in menu

--- scripts/menus/mainmenu.gd
+++ scripts/menus/mainmenu.gd
@@ -3,31 +3,65 @@ extends MarginContainer
 @onready var mainMenuContainer: HBoxContainer = $HBoxContainer
 @onready var settingsContainer: SettingsMenu = $Settings
 
+@onready var buttons: Array[Button] = [
+	$HBoxContainer/VSplitContainer/VBoxContainer/NewGame,
+	$HBoxContainer/VSplitContainer/VBoxContainer/Continue,
+	$HBoxContainer/VSplitContainer/VBoxContainer/Options,
+	$HBoxContainer/CreditsContainer/Credits,
+	$HBoxContainer/QuitContainer/Quit
+]
+var focusedButtonIndex = 0
+
 func _ready():
 	settingsContainer.returnCallback = onReturn
+	buttons[focusedButtonIndex].grab_focus()
+
+func _input(event):
+	if mainMenuContainer.visible:
+		if Input.is_action_pressed("ui_down"):
+			focusedButtonIndex += 1
+			if focusedButtonIndex >= len(buttons):
+				focusedButtonIndex -= len(buttons)
+			buttons[focusedButtonIndex].grab_focus()
+			get_viewport().set_input_as_handled()
+		
+		if Input.is_action_pressed("ui_up"):
+			focusedButtonIndex -= 1
+			if focusedButtonIndex < 0:
+				focusedButtonIndex += len(buttons)
+			buttons[focusedButtonIndex].grab_focus()
+			get_viewport().set_input_as_handled()
+		
 
 func new_game():
+	focusedButtonIndex = 0
 	print("new game clicked!")
 	#TODO implement navigation and new game functionality
 
 func continue_():
+	focusedButtonIndex = 1
 	print("continue clicked!")
 	#TODO implement navigation and continue functionality
 
 func options():
+	focusedButtonIndex = 2
 	settingsContainer.visible = true
+	settingsContainer.onShow()
 	mainMenuContainer.visible = false
 
 func credits():
+	focusedButtonIndex = 3
 	print("credits clicked!")
 	#TODO implement navigation and credits functionality
 
 func quit():
+	focusedButtonIndex = 4
 	get_tree().quit()
 	
 func onReturn():
 	settingsContainer.visible = false
 	mainMenuContainer.visible = true
+	buttons[focusedButtonIndex].grab_focus()
 
 func _on_new_game_button_up():
 	new_game()
--- scripts/menus/settings_menu_panel.gd
+++ scripts/menus/settings_menu_panel.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Control
 
 class_name SettingsMenu
 
@@ -8,7 +8,32 @@ class_name SettingsMenu
 
 @onready var returnButton: Button = $Return
 
+@onready var focusable: Array[Control] = [
+	masterSlider, musicSlider, SFXSlider, returnButton
+]
+
+var focusedButtonIndex = 0
+
 var returnCallback: Callable
+
+func _input(event):
+	if self.visible:
+		if Input.is_action_pressed("ui_cancel"):
+			_on_return_button_up()
+			get_viewport().set_input_as_handled()
+		if Input.is_action_pressed("ui_down"):
+			focusedButtonIndex += 1
+			if focusedButtonIndex >= len(focusable):
+				focusedButtonIndex -= len(focusable)
+			focusable[focusedButtonIndex].grab_focus()
+			get_viewport().set_input_as_handled()
+		
+		if Input.is_action_pressed("ui_up"):
+			focusedButtonIndex -= 1
+			if focusedButtonIndex < 0:
+				focusedButtonIndex += len(focusable)
+			focusable[focusedButtonIndex].grab_focus()
+			get_viewport().set_input_as_handled()
 
 func _ready():
 	masterSlider.value = SaveHandler.settings.master
@@ -16,14 +41,22 @@ func _ready():
 	SFXSlider.value = SaveHandler.settings.sfx
 
 func _on_master_volume_slider_value_changed(value):
+	focusedButtonIndex = 0
 	SaveHandler.setMasterVolume(value)
 func _on_music_volume_slider_value_changed(value):
+	focusedButtonIndex = 1
 	SaveHandler.setMusicVolume(value)
 func _on_sfx_volume_slider_value_changed(value):
+	focusedButtonIndex = 2
 	SaveHandler.setSFXVolume(value)
 func _on_return_button_up():
 	if returnCallback == null:
 		print("Menu hosting this settings panel does not set 'returnCallback' parameter of SettingsMenu node.")
 	else:
 		returnCallback.call()
+		
+func onShow():
+	focusedButtonIndex = 0
+	focusable[focusedButtonIndex].grab_focus()
+	
 		
